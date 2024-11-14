289cf8324087d56b130f7013959daffa3966056c
Orlando Avila <orlando.avilag@hotmail.com>
2024-11-14 10:13:04 -0600

    Testing parallel execution

--- src/main/java/org/lando/utils/ConstantData.java
+++ src/main/java/org/lando/utils/ConstantData.java
@@ -7,6 +7,6 @@ public class ConstantData {
     public static String deviceName = "Pixel 5";
     public static String appPackage = "com.wdiodemoapp";
     public static String appActivity = "com.wdiodemoapp.MainActivity";
-    public static String androidApp = "bs://c2f21d0b9d3c3e42e24c744d816052bf8a878787";
+    public static String androidApp = "bs://13f88fbc4233bdf46100a0c824ae815d1e8c2250";
     public static String iosApp = "bs://c2f21d0b9d3c3e42e24c744d816052bf8a878787";
 }
